ce4cd572a94de320a425abcb8ca49671a822245b
Konstantin <50913460+matusdev@users.noreply.github.com>
2020-02-03 04:32:06 +0200

    bazabet

---
+++ Bazabet/js/main.js
@@ -0,0 +1,38 @@
+/*!
+ * project-name v0.0.1
+ * A description for your project.
+ * (c) 2020 YOUR NAME
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+// function parallax(event) {
+//     this.querySelectorAll('.layer').forEach(layer => {
+//         let speed = layer.getAttribute('data-speed');
+//         layer.style.transform = 'translateX(${event.clientX*speed}px)'
+//     });
+// }
+// document.addEventListener('mousemove', parallax);
+
+$(document).ready((function () {
+    window.onload = function() {
+        setTimeout((function(){
+            $('.getBonus__images').addClass('start-animation');
+        }));
+    };
+
+    if (jQuery(window).width() > 1199) {
+        $(window).on('mousemove resize', (function (e) {
+            var w = $(window).width();
+            var h = $(window).height();
+            var offsetX = 0.5 - e.pageX / w;
+            $(".parallax").each((function (i, el) {
+                var offset = parseInt($(el).data('offset'));
+                var translate = "translateX(" + Math.round(offsetX * offset) + "px";
+                $(el).css({
+                    'transform': translate
+                });
+            }));
+        }));
+    }
+}));
